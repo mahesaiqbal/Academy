7d3a97007296a67e0979e7aad4bb71e0d69caf27
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2019-08-24 11:20:52 +0700

    Supports multiscreen

--- app/src/main/java/com/mahesaiqbal/academy/ui/reader/CourseReaderActivity.kt
+++ app/src/main/java/com/mahesaiqbal/academy/ui/reader/CourseReaderActivity.kt
@@ -2,19 +2,26 @@ package com.mahesaiqbal.academy.ui.reader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mahesaiqbal.academy.R
+import com.mahesaiqbal.academy.data.source.local.entity.ModuleEntity
 import com.mahesaiqbal.academy.ui.reader.content.ModuleContentFragment
 import com.mahesaiqbal.academy.ui.reader.list.ModuleListFragment
 import com.mahesaiqbal.academy.viewmodel.ViewModelFactory
+import com.mahesaiqbal.academy.vo.Resource
+import com.mahesaiqbal.academy.vo.Status.*
+import kotlinx.android.synthetic.main.activity_course_reader.*
 
 class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
 
     lateinit var courseReaderViewModel: CourseReaderViewModel
+
+    var isLarge = false
 
     companion object {
         fun obtainViewModel(activity: FragmentActivity): CourseReaderViewModel {
@@ -27,13 +34,20 @@ class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_reader)
 
+        supportActionBar?.title = "Course Reader"
+
+        if (frame_list != null) {
+            isLarge = true
+        }
+
         courseReaderViewModel = obtainViewModel(this)
+
+        courseReaderViewModel.modules.observe(this, courseReader)
 
         val bundle: Bundle? = intent.extras
         if (bundle != null) {
             var courseId = bundle.getString("extra_course_id")
             if (courseId != null) {
-                Log.d("courseIdReader", courseId)
                 courseReaderViewModel.setCourseIdValue(courseId)
                 populateFragment()
             }
@@ -41,11 +55,13 @@ class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
     }
 
     override fun moveTo(position: Int, moduleId: String) {
-        val fragment: Fragment = ModuleContentFragment.newInstance()
-        supportFragmentManager.beginTransaction()
-            .add(R.id.frame_container, fragment, ModuleContentFragment.newInstance().tag)
-            .addToBackStack(null)
-            .commit()
+        if (!isLarge) {
+            val fragment: Fragment = ModuleContentFragment.newInstance()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.frame_container, fragment, ModuleContentFragment.newInstance().tag)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     override fun onBackPressed() {
@@ -58,14 +74,78 @@ class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
 
     private fun populateFragment() {
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        var fragment: Fragment? = supportFragmentManager.findFragmentByTag(ModuleListFragment.newInstance().tag)
 
-        if (fragment == null) {
-            fragment = ModuleListFragment.newInstance()
-            fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.newInstance().tag)
-            fragmentTransaction.addToBackStack(null)
+        if (!isLarge) {
+            var fragment: Fragment? = supportFragmentManager.findFragmentByTag(ModuleListFragment.newInstance().tag)
+
+            if (fragment == null) {
+                fragment = ModuleListFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.newInstance().tag)
+                fragmentTransaction.addToBackStack(null)
+            }
+
+            fragmentTransaction.commit()
+        } else {
+            var fragmentList = supportFragmentManager.findFragmentByTag(ModuleListFragment.newInstance().tag)
+
+            if (fragmentList == null) {
+                fragmentList = ModuleListFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_list, fragmentList, ModuleListFragment.newInstance().tag)
+            }
+
+            var fragmentContent = supportFragmentManager.findFragmentByTag(ModuleContentFragment.newInstance().tag)
+
+            if (fragmentContent == null) {
+                fragmentContent = ModuleContentFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_content, fragmentContent, ModuleContentFragment.newInstance().tag)
+            }
+
+            fragmentTransaction.commit()
         }
+    }
 
-        fragmentTransaction.commit()
+    private val courseReader = Observer<Resource<List<ModuleEntity>>> { modules ->
+        if (modules != null) {
+            when (modules.status) {
+                LOADING -> {  }
+                SUCCESS -> {
+                    if (modules.data != null && modules.data.size > 0) {
+                        val firstModule = modules.data[0]
+                        val isFirstModuleRead = firstModule.read
+
+                        if (!isFirstModuleRead) {
+                            val firstModuleId = firstModule.moduleId
+                            courseReaderViewModel.setSelectedModule(firstModuleId)
+                        } else {
+                            val lastReadModuleId = getLastReadFromModules(modules.data)
+                            if (lastReadModuleId != null) {
+                                courseReaderViewModel.setSelectedModule(lastReadModuleId)
+                            }
+                        }
+                    }
+                    removeObserver()
+                }
+                ERROR -> {
+                    Toast.makeText(this, "Gagal menampilkan data.", Toast.LENGTH_SHORT).show()
+                    removeObserver()
+                }
+            }
+        }
+    }
+
+    private fun getLastReadFromModules(moduleEntities: List<ModuleEntity>): String? {
+        var lastReadModule: String? = null
+        for (moduleEntity in moduleEntities) {
+            if (moduleEntity.read) {
+                lastReadModule = moduleEntity.moduleId
+                continue
+            }
+            break
+        }
+        return lastReadModule
+    }
+
+    fun removeObserver() {
+        courseReaderViewModel.modules.removeObserver(courseReader)
     }
 }
